498eee876e2499aef73039a26ae2ddeaa2cf87f7
charles madhuku <35784564+dastiles@users.noreply.github.com>
2023-05-28 21:07:32 +0200

    updated db file

--- config/db.js
+++ config/db.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
 
+let url
+if (process.env.NODE_ENV === 'development') {
+    url = process.env.MONGO_URL_DEV
+} else {
+    url = process.env.MONGO_URL_PROD
+}
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL)
+        const conn = await mongoose.connect(url)
         console.log(`MongoDb Connected : ${conn.connection.host}`.cyan.underline)
     } catch (error) {
         console.log(error)
